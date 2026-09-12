f354bc93b3c37b9468be03e92aaf18f685af023f
Data_C0re_ <10966796+Rzbck@users.noreply.github.com>
2026-09-12 17:36:32 +0200

    style(icons): load Supericons Lucide theme

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -9,6 +9,7 @@ import "./discovery-auto.css";
 import "./discovery-v2.css";
 import "./discovery-v3.css";
 import "./polish.css";
+import "./icons.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
