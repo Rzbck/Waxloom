62a812875450047b0cfbbb1e7bf345edb87dd663
Data_C0re_ <10966796+Rzbck@users.noreply.github.com>
2026-09-12 13:16:36 +0200

    style(discovery): load automatic recommendation layout

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import { PlayerProvider } from "./Player";
 import "./styles.css";
 import "./performance.css";
+import "./discovery-auto.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
