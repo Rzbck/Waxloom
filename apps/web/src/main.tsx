cfad0d3aa865b14ab57ddaec5c318d7b1d1a829b
Data_C0re_ <10966796+Rzbck@users.noreply.github.com>
2026-09-13 08:11:38 +0200

    fix(web): load sidebar and cache migration hotfixes

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "./browser-cache-v8";
 import App from "./App";
 import { PlayerProvider } from "./Player";
 import "./styles.css";
@@ -17,6 +18,7 @@ import "./layout-polish-v4.css";
 import "./icon-centering-v5.css";
 import "./discovery-shelf-stability-v6.css";
 import "./player-clearance-v7.css";
+import "./sidebar-fixed-v8.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
