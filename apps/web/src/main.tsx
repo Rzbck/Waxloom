3081bd5cffd23a81576f4d336345a0a5c09588ca
Data_C0re_ <10966796+Rzbck@users.noreply.github.com>
2026-09-13 08:06:00 +0200

    fix(web): load fixed-player clearance last

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -16,6 +16,7 @@ import "./discovery-icons-hotfix.css";
 import "./layout-polish-v4.css";
 import "./icon-centering-v5.css";
 import "./discovery-shelf-stability-v6.css";
+import "./player-clearance-v7.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
